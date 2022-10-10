7a44cfccb5c1b3d8677c02f399a1992bfb1895da
Gabriel Kremer <gajokremer@gmail.com>
2022-10-09 19:48:55 -0500

    First commit.

---
+++ pages/index.jsx
@@ -0,0 +1,34 @@
+// import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+import ProfileBar from "../components/ProfileBar";
+import Schedule from "../components/Schedule";
+
+export default function Home() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No table data</p>;
+
+  // console.log("Data");
+  // console.log(data);
+
+  return (
+    <div>
+      <Header></Header>
+      <ProfileBar></ProfileBar>
+      <Schedule props={data} week={1}></Schedule>
+    </div>
+  );
+}
